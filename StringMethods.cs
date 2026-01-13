c8ab0c3784324dd8f25415b9ab68b37c495f1ebb
Hannah J-H <h.jerminghavill@gmail.com>
2026-01-13 14:34:08 +0100

    class with methods

---
+++ StringMethods.cs
@@ -0,0 +1,25 @@
+public class StringMethods
+{
+    public static string ReverseString(string input)
+    {
+        if (input == null)
+            return null;
+
+        char[] charArray = input.ToCharArray();
+        System.Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    public static string MergeStrings(string str1, string str2)
+    {
+        return str1 + str2;
+    }
+
+    public static string RemoveWhitespace(string input)
+    {
+        if (input == null)
+            return null;
+
+        return string.Concat(input.Where(c => !char.IsWhiteSpace(c)));
+    }
+}
